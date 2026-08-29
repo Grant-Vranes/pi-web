7d7984460a6ddfc8487f850212dda8de1a4e6695
hshi22 <hao.shi4@dxc.com>
2026-08-29 10:08:49 +0800

    Widen main chat column from 820px to 1100px to reduce side whitespace

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1458,7 +1458,7 @@ export const ChatInput = forwardRef<ChatInputHandle, Props>(function ChatInput({
           e.target.value = "";
         }}
       />
-      <div style={{ maxWidth: 820, margin: "0 auto" }}>
+      <div style={{ maxWidth: 1100, margin: "0 auto" }}>
         <ModelErrorBanner error={modelError} />
         <ModelScopeWarningBanner warnings={modelScopeWarnings} />
         {/* Queued steering / follow-up messages (delivered by pi on upcoming turns) */}
